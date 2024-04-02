aa4bd6a07a60272358f2447d8872ef3e6e59f3e4
wesley de campos rodrigues <wesleyc21rodrigues@gmail.com>
2024-04-02 19:44:10 -0300

    ex.001

---
+++ index.js
@@ -0,0 +1,10 @@
+var nome = prompt("Insira o seu nome");
+var num1 = 2;
+var num2 = prompt("Insira o numero 2");
+var mensagem1 = prompt("Insira a sua mensagem de boas vindas")
+var mensagem2 = prompt("Insira a sua mensagem de despedida")
+var resultado = parseInt(num1) + parseInt(num2)
+
+console.log( "Olá, seja bem vindo" + " " + nome );
+console.log( num1 + ' + ' + num2 + ' = ' + resultado);
+alert ( mensagem1 + ',' + ' ' + mensagem2 )
